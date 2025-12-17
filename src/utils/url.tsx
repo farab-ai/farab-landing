941039cbc36c30f01248eed338d6b518c9c672f2
DYernar <eduisebai.1@gmail.com>
2025-12-17 01:42:06 +0100

    fix apihost

--- src/utils/url.tsx
+++ src/utils/url.tsx
@@ -1,3 +1,3 @@
 // 🌐 API Configuration
-export const APIHOST = "http://localhost:8080";
-// export const APIHOST = "https://farab-api-335163170244.us-central1.run.app";
+// export const APIHOST = "http://localhost:8080";
+export const APIHOST = "https://api.farab.xyz";
